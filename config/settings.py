4094757a0080b0c1a9e9478f6987ebe533241f7d
Asadbek <asadbekoffical1202@gmail.com>
2025-05-06 14:28:16 +0500

    Update; add gunicorn

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static'] 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
